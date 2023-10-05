53ee9c4ef166c72249a019d91b2643dc64eb1ffe
itsmeeBazz <saktiawanabas@gmail.com>
2023-10-05 15:05:42 +0700

    create Register View

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,1 +1,46 @@
-<h1>Test</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Register</title>
+</head>
+<body>
+        <div class="container">
+          <div class="position-absolute top-50 start-50 translate-middle">
+            <div class="card " style="width: 22rem">
+                <div class="card-header text-center">
+                  Daftar
+                </div>
+                <div class="card-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="username" class="form-label">username</label>
+                            <input placeholder="Masukkan Username" type="name" class="form-control" id="username" aria-describedby="emailHelp">
+                          </div>
+                          <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                            <input placeholder="Masukkan Email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                          </div>
+                          <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input placeholder="Masukkan Password" type="password" class="form-control" id="exampleInputPassword1">
+                          </div>
+                          <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
+                            <input placeholder="Confirm Password" type="password" class="form-control" id="exampleInputPassword1">
+                          </div>
+                          <button type="submit" class="btn btn-primary">Daftar</button>
+                    </form>
+                </div>
+              </div>
+            </div>
+        </div>
+            
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
